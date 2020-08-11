1e6deb890ad05fc723d6fe8b97137285ee21520c
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-11 14:49:26 +0900

    introduce prettier and tslint

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
-import net from 'net'
+import net from 'net';
 
 const client = new net.Socket();
-const port = 80
-const host = "artree.co.jp"
-const request = "GET / HTTP/1.1\r\nHost: localhost:5000\r\nUser-Agent: curl/7.54.0\r\nAccept: */*\r\n\r\ntest"
+const port = 80;
+const host = 'artree.co.jp';
+const request =
+    'GET / HTTP/1.1\r\nHost: localhost:5000\r\nUser-Agent: curl/7.54.0\r\nAccept: */*\r\n\r\ntest';
 client.setEncoding('utf8');
 
 const get = () => {
     client.connect(port, host, () => {
-        console.log("send")
+        console.log('send');
         client.write(request);
-    })
+    });
 
-    client.on('data', (data :any) => {
-        console.log("Data is following" + data);
+    client.on('data', (data: any) => {
+        console.log('Data is following' + data);
         client.destroy();
     });
 
     client.on('close', () => {
         console.log('Connection closed');
     });
-}
+};
 
-get()
+get();
